0ee06ea7d2bb56d48986798c9e75afc7b0db0a71
unknown <wisnukm41@gmail.com>
2020-03-27 17:06:42 +0700

    first commit

---
+++ src/kelasUtama.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author whiz
+ */
+public class kelasUtama {
+    public static void main(String[] args) {
+//        frameUtama FU = new frameUtama();
+//        FU.tampil("PROGRAM PERTAMA GUI");
+          therealutama tru = new therealutama();
+          tru.setVisible(true);
+    }
+}
